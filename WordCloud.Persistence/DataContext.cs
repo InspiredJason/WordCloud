e800d2d84778d1a66cd332f8cd69abdfa65f45be
Jason Richards <Jason.Richards@trustpower.co.nz>
2020-06-10 22:21:23 +1200

    Add DataContext to Persistence project and create initial migration

---
+++ WordCloud.Persistence/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using WordCloud.Domain;
+
+namespace WordCloud.Persistence
+{
+    public class DataContext : IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
