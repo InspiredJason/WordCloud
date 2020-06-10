640a046e7ad9a030547490049f61a3abc8f833b6
Jason Richards <Jason.Richards@trustpower.co.nz>
2020-06-10 22:18:30 +1200

    Add WordCount domain class

---
+++ WordCloud.Domain/WordCount.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WordCloud.Domain
+{
+    public class WordCount
+    {
+        public string Id { get; set; }
+        [Required]
+        [Column(TypeName = "varchar(50)")]
+        public string Word { get; set; }
+        public int Count { get; set; }
+    }
+}
