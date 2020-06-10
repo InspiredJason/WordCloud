2915d1a760360f11d450095e1d115e911d187ab6
Jason Richards <Jason.Richards@trustpower.co.nz>
2020-06-10 22:48:11 +1200

    Add test for generated salted hash

---
+++ WordCloud.Tests/EncryptionTests.cs
@@ -0,0 +1,21 @@
+﻿using WordCloud.Infrastructure;
+using Xunit;
+
+namespace WordCloud.Tests
+{
+    public class EncryptionTests
+    {
+        [Fact]
+        public void ValidateSaltedHashDiffersForMultipleCallsWithSameTestString()
+        {
+            // Arrange
+            var testString = "TestThis";
+
+            // Act
+            var result1 = Encryption.GenerateSaltedHash(testString);
+            var result2 = Encryption.GenerateSaltedHash(testString);
+
+            Assert.NotEqual(result1, result2);
+        }
+    }
+}
